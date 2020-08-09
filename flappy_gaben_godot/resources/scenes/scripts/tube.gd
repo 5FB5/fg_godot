4fb343fae7700cf3c799e9b7ff3d7c69640cf961
5FB5 <vecktor911@gmail.com>
2020-08-09 16:13:23 +0300

    added basic gameplay

--- flappy_gaben_godot/resources/scenes/scripts/tube.gd
+++ flappy_gaben_godot/resources/scenes/scripts/tube.gd
@@ -2,21 +2,28 @@ extends Area2D
 
 export var tubeSpeed = 400
 
+#Basic movement for tube
 func _process(delta):
 	position.x -= tubeSpeed * delta
 	pass
 
+#If tube exited screen then just delete
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	print ("Game: Tube deleted")
 	pass
 
+#If coin trigger collides with player then we increase score
 func _on_Area2D_body_entered(body):
-	print("Game: Player score +1")
+	var playerVars = get_node("/root/PlayerVars")
+	if (playerVars.canFly == true):	
+		print("Game: Player score +1")
 	pass
 
+#If tube collides with player then we set player canFly var to false (defeat)
 func _on_RigidBody2D_body_entered(body):
-	var _playerVars = get_node("/root/PlayerVars")	
-	_playerVars.canFly = false	
-	print("Game: player.canFly = ", _playerVars.canFly)
+	var playerVars = get_node("/root/PlayerVars")	
+	playerVars.canFly = false	
+	
+	print("Game: player.canFly = ", playerVars.canFly)
 	pass
--- flappy_gaben_godot/resources/scenes/scripts/tubeSpawner.gd
+++ flappy_gaben_godot/resources/scenes/scripts/tubeSpawner.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
+#We should see this node's sprite only in editor
 func _ready():
-	$Sprite.hide() # sprite is only for editor mode
-	pass
-	
-func spawnTube():
-	var newTube = load("res://resources/scenes/tube.tscn").instance()
-	add_child(newTube)
-	newTube.position = Vector2(2144, rand_range(255, 590))
-	newTube.add_to_group("Tubes")
+	$Sprite.hide()
 	pass
 
+#Main spawning func that load tube's instance and set his position	
+func spawnTube():
+	var newTube = preload("res://resources/scenes/tube.tscn").instance()
+	
+	add_child(newTube)
+	
+	newTube.position = Vector2(2144, rand_range(255, 590))
+	newTube.add_to_group("Tubes")
+	
+	pass
+
+#Timer function that activate spawnTube() func at a specific time
 func _on_Timer_timeout():
 	spawnTube()
 	print ("Game: New tube spawned")
+	
 	pass
