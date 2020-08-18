860ba97f3774bd8ab8a2325012f3f111d57cc025
5FB5 <vecktor911@gmail.com>
2020-08-18 14:34:23 +0300

    added game over screen (just scene)

--- flappy_gaben_godot/resources/scenes/scripts/ui_pause.gd
+++ flappy_gaben_godot/resources/scenes/scripts/ui_pause.gd
@@ -1,12 +1,16 @@
 extends Control
 
+onready var _globalVar = get_node("/root/GlobalVar")
+
 func _input(event):
-	if (event.is_action_pressed("game_pause")):
-		get_tree().paused = not get_tree().paused
-		visible = not visible
+	#Turn on pause menu if player has not lost
+	if (_globalVar.playerCanFly == true):
+		if (event.is_action_pressed("game_pause")):
+			get_tree().paused = not get_tree().paused
+			visible = not visible
 		
-		if (visible):
-			print("Game: Paused")
-		else:
-			print("Game: Resumed")
+			if (visible):
+				print("Game: Paused")
+			else:
+				print("Game: Resumed")
 	pass
