4861cb38bed83293a0c56b4d575a97df2a385c2a
5FB5 <vecktor911@gmail.com>
2020-12-01 14:11:27 +0300

    Changed best score font color, main title font color, little changes for splash text

--- flappy_gaben_godot/resources/scenes/ui/labelJokes.gd
+++ flappy_gaben_godot/resources/scenes/ui/labelJokes.gd
@@ -2,7 +2,7 @@
 extends Label
 
 func _ready():
-	var splash = ['!!!1!', 'Now with V-Sync!', 'Endless!', 'Not inspired by Flappy Bird!', 'by 5FB5 :3', 'LOL!', 'OpenGL ES3!', 'I hope it was worth the wait!', '0% money!', 'Multithreaded!', 'I dont think Valve will see it!', 'Made with Godot!', 'misingno!', 'uuddlrlrbastart', '0x10c!', 'Minecraft is my life!', 'i luv gamejolt', 'A joke out of control!', 'Waiting for Godot 4.0!', 'Without raytracing!', 'How about hot coffee?', 'Greetings!', '[Funny joke here]!', 'Freeman, you fool!', 'Haha, LEL', 'Almost original game!', 'No skins!', 'Made using code!', 'Check it out!', 'IDDQD!', 'HL3!', 'Not made with Unity!', 'Check VNN!']
+	var splash = ['How about some Prutsky?', 'Now with V-Sync!', 'Endless!', 'Not inspired by Flappy Bird!', 'by 5FB5 :3', 'LOL!', 'OpenGL ES3!', 'I hope it was worth the wait!', '0% money!', 'Multithreaded!', 'I dont think Valve will see it!', 'Made with Godot!', 'misingno!', 'uuddlrlrbastart', '0x10c!', 'Minecraft is my life!', 'I love GameJolt!', 'A joke out of control!', 'Waiting for Godot 4.0!', 'Without raytracing!', 'Hot coffee?', 'Greetings!', '[Funny splash text here]!', 'Freeman, you fool!', 'Haha, LEL!', 'Almost original game!', 'No skins!', 'Made using code!', 'Check it out!', 'IDDQD!', 'HL3!', 'Not made with Unity!', 'Check VNN!']
 	randomize()
 	var current_text = splash[rand_range(0, splash.size())]
 	self.text = current_text
